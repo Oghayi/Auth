d85be469b2735e450868c2aa2e463929d43f6f7f
Mus'ab <oghayidflash1008@gmail.com>
2026-02-07 00:49:24 +0100

    CRUD for User

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -80,8 +80,24 @@ const loginUser = async (req, res) => {
    res.status(200).json({ message: "Logged out successfully"});
 }
 
+const updateUser = async (req, res) => {
+    try {
+        if (Object.keys(req.body).length === 0) {
+            return res.status(400).json({ message: "No data provided" });
+        }
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({message: "User updated successfully", user});
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error: error });
+    }
+}
+
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    updateUser
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router} from 'express';
-import { loginUser, registerUser, logoutUser } from '../controller/auth.controller.js';
+import { loginUser, registerUser, logoutUser, updateUser } from '../controller/auth.controller.js';
 const router = Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').post(logoutUser);
+router.route('/update').put(updateUser);
 
 export default router;
