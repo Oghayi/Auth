7456c57e0595e5ce6d897f1a2c80a9fbfbb0803f
Mus'ab <oghayidflash1008@gmail.com>
2026-02-07 01:02:05 +0100

    More correction on CRUD

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -78,11 +78,8 @@ const loginUser = async (req, res) => {
 
 const getUser = async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        res.status(200).json({ user });
+        const users = await User.find();
+        res.status(200).json({ users });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error });
     }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,10 @@
 import { Router} from 'express';
-import { loginUser, registerUser, logoutUser, updateUser, getUser } from '../controller/auth.controller.js';
+import { loginUser, registerUser, logoutUser, updateUser, getUsers } from '../controller/auth.controller.js';
 const router = Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
-router.route('/getUser/:id').get(getUser);
+router.route('/getUsers').get(getUser);
 router.route('/logout').post(logoutUser);
 router.route('/update/:id').put(updateUser);
 
